47d89d0694921a8fe3d7e7b0e0352e6a4703d4cd
abhilash6952 <116501668+abhilash6952@users.noreply.github.com>
2024-08-26 22:21:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 aws s3 cp s3://java-artifacts-devops4solutions1/Sample-Demo-Java-App/LoginWebApp-1.0.0.war /tmp
 sudo mv /tmp/LoginWebApp-1.0.0.war /opt/apache-tomcat-9.0.93/webapps/
-sudo cd /opt/apache-tomcat-9.0.93/bin/
-sudo bash startup.sh
-
+sudo chmod +x /opt/apache-tomcat-9.0.93/bin/./startup.sh
