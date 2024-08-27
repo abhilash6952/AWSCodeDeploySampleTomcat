53af94e05454125744e3ab5f3dc48bac3bd49ca0
devopscloudsre <59342234+devopscloudsre@users.noreply.github.com>
2024-08-27 12:42:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://java-artifacts-devops4solutions1/Sample-Demo-Java-App/LoginWebApp-1.0.0.war /tmp
+#aws s3 cp s3://java-artifacts-devops4solutions1/Sample-Demo-Java-App/LoginWebApp-1.0.0.war /tmp
 sudo mv /tmp/LoginWebApp-1.0.0.war /opt/apache-tomcat-9.0.93/webapps/
 sudo chmod +x /opt/apache-tomcat-9.0.93/bin/./startup.sh
